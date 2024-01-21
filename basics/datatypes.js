3155f6129e83077b707b344213faaf749f0350f3
Bikramjit Jena <algo.bikram@gmail.com>
2024-01-21 14:18:54 +0000

    changes about heap and stack

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -4,7 +4,7 @@
 //Primitive 
 
 // 7 types : String,Number,Boolean,null,undefined,Symbol,BigInt
-//(Call by Value) All these datatypes 
+//(Call by Value) All these datatypes ,uses Stack
 const score = 100
 const scoreValue = 100.3
 const isLoggedIn = false
@@ -14,7 +14,7 @@ const anotherId= Symbol('123')
 console.log(id===anotherId)
 //Reference (Non Primitive)
 
-//Array,Objects,Functions
+//Array,Objects,Functions uses Heap
 const heros = ["bikram","Jena", "Jit"]
 console.log(typeof heros)
 let myObj = {
@@ -24,3 +24,18 @@ let myObj = {
 const myfunction = function(){
     console.log("Hello world")
 }
+
+
+let name = "Bikram"
+let name2 = name
+name2="Amrita"
+console.log(name2)
+//for heap memory explanation
+let userOne = {
+    email:"bikramjitj@gmail.com",
+    age:24
+}
+let userTwo = userOne
+userTwo.email = "vickymalhotra@yahoo.com"
+console.log(userOne.email)
+console.log(userTwo.email)
